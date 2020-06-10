c5376d199f8c62d994d01e93a83658626c9e3338
hendrywang <hendrywang@gmail.com>
2020-06-10 13:46:42 +0700

    modify

--- Assets/Scripts/MonoBehaviours/Camera/CameraFollowPlayer.cs
+++ Assets/Scripts/MonoBehaviours/Camera/CameraFollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollowPlayer : MonoBehaviour
 {
     public GameObject player;
-	private Vector3 offset = new Vector3(0, 3, -7);
+	public Vector3 offset = new Vector3(0, 3, -7);
 
     // Start is called before the first frame update
     void Start()
